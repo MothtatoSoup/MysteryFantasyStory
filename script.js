1898a6bbd67b92fe15a654f3f642b3c22d861281
MothtatoSoup <maddy.puryear@outlook.com>
2025-02-19 11:30:59 -0500

    Moved stuff around, Added text to the storytext

--- script.js
+++ script.js
@@ -3,17 +3,17 @@
 // ---------------------------------
 
 // ---- STORY CONTENT ----
-let TheScene = 0; // What scene it is on
 let StoryTitle = document.getElementById("Title"); // The Title of the page
 let SceneTitle = document.getElementById("STitle"); // The title of the scene
 
 let StoryText = document.getElementById("SText"); // The story text
-const STxtList = ["Beggining Text", "Omg a change!!"]; // List of content for the story text
+const STxtList = ["At the corner of Elven St lies a small cafe emitting the heavenly smell of coffee. <br> e ", "Omg a change!!"]; // List of content for the story text
 
 let SceneImage = document.getElementById("image"); // The images displayed
 const StoryImages = ["FROG.jpg"] // List of images for the scene
 
 // THE SCENE
+let TheScene = 0; // What scene it is on
 function SceneNum(num){
     function ChangeStoryText(num) { // CHanges text based on scene
         StoryText.textContent = STxtList[num];
@@ -50,8 +50,8 @@ function isValidInput(input) { // Checks if the inputed value is correct
 }
 
 
-
-
+// S T O R Y     C O D E
+// ---------------------------------
 
 
 /* For Loop. Include at least one for loop in your story.
